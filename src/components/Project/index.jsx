f763e9c11b972890b1d159e893236e6ad79a2e98
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2019-10-21 22:25:07 -0400

    Created Project component.

---
+++ src/components/Project/index.jsx
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import styled from 'styled-components';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Div = styled.div``;
+const GitHubLink = styled.a``;
+const LiveLink = styled.a``;
+const ProjectTitle = styled.ProjectTitle``;
+
+Project.propTypes = {
+  children: PropTypes.node.isRequired,
+  githubSlug: PropTypes.string.isRequired,
+  image: PropTypes.node.isRequired,
+  liveUrl: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default function Project({
+  children,
+  githubSlug,
+  image,
+  liveUrl,
+  name,
+  ...restProps
+}) {
+  return (
+    <Div {...restProps}>
+      <ProjectTitle>{name}</ProjectTitle>
+      {image}
+      <GitHubLink
+        href={`https://github.com${githubSlug}`}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <FontAwesomeIcon icon={faGithub} /> Visit GitHub repository{' '}
+        <FontAwesomeIcon icon={faExternalLinkAlt} />
+      </GitHubLink>
+      <LiveLink href={liveUrl} rel="noopener noreferrer" target="_blank">
+        Visit live project <FontAwesomeIcon icon={faExternalLinkAlt} />
+      </LiveLink>
+      {children}
+    </Div>
+  );
+}
