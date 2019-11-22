0ce676a849b9b44f6f48e365dc752c429b03b5b8
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2019-11-22 13:52:48 -0500

    Style contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -3,41 +3,96 @@ import styled from 'styled-components';
 import { faSatelliteDish } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { Button, Layout, SEO } from '../components';
+import { Button, Layout, SEO, StarCanvas } from '../components';
+import { variables as styleVariables } from '../components/GlobalStyle';
 
 const BlockButton = styled(Button)`
   display: block;
-  margin-left: auto;
+  font-size: ${styleVariables.h4.xs.rem}rem;
+  line-height: ${styleVariables.h4.xs.lh};
+  margin: 0 auto ${2 * styleVariables.baselinePixels}px auto;
   white-space: nowrap;
+  width: 100%;
+
+  @media (min-width: ${styleVariables.breakpoint.sm}px) {
+    margin: 0 0 0 auto;
+    width: auto;
+  }
+
+  @media (min-width: ${styleVariables.breakpoint.md}px) {
+    font-size: ${styleVariables.h4.md.rem}rem;
+    line-height: ${styleVariables.h4.md.lh};
+  }
+
+  @media (min-width: ${styleVariables.breakpoint.lg}px) {
+    font-size: ${styleVariables.h4.lg.rem}rem;
+    line-height: ${styleVariables.h4.lg.lh};
+  }
 `;
-const Form = styled.form`
-  border: 1px solid white;
-  border-radius: 0.25rem;
-  padding: 1rem;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0 auto;
+  max-width: 33em;
 `;
+
+const Form = styled.form``;
 const FormGroup = styled.div`
   display: flex;
   flex-direction: column;
-  margin-bottom: 1rem;
+  margin-bottom: ${2 * styleVariables.baselinePixels}px;
+  &:last-of-type {
+    margin-bottom: ${4 * styleVariables.baselinePixels}px;
+  }
 `;
+
 const Input = styled.input`
-  background: none;
+  background: black;
   border: 1px solid white;
   border-radius: 0.25rem;
   color: white;
-  padding: 0.5rem;
+  font-size: ${styleVariables.body.xs.rem}rem;
+  line-height: ${styleVariables.body.xs.lh};
+  padding: ${styleVariables.baselinePixels - 1}px;
 `;
+
 const Label = styled.label`
-  font: 1.25rem/1.6 'Fira Mono', monospace;
+  font-family: 'Fira Mono', monospace;
+  font-size: ${styleVariables.h5.xs.rem}rem;
+  line-height: ${styleVariables.h5.xs.lh};
+
+  @media (min-width: ${styleVariables.breakpoint.md}px) {
+    font-size: ${styleVariables.h5.md.rem}rem;
+    line-height: ${styleVariables.h5.md.lh};
+  }
+
+  @media (min-width: ${styleVariables.breakpoint.lg}px) {
+    font-size: ${styleVariables.h5.lg.rem}rem;
+    line-height: ${styleVariables.h5.lg.lh};
+  }
 `;
+
 const Paragraph = styled.p`
-  font-size: 1.25rem;
-  line-height: 1.6;
-  margin-bottom: 3rem;
+  font-size: ${styleVariables.h5.xs.rem}rem;
+  line-height: ${styleVariables.h5.xs.lh};
+  margin-bottom: ${2 * styleVariables.baselinePixels}px;
+  max-width: 33em;
+`;
+
+const Section = styled.section`
+  margin: 0 auto;
   max-width: 33em;
 `;
 const StyledIcon = styled(FontAwesomeIcon)`
   margin-right: 1rem;
+`;
+const StyledStarCanvas = styled(StarCanvas)`
+  left: 0;
+  position: absolute;
+  top: 0;
+  z-index: -1;
 `;
 const Title = styled.h1`
   font-family: 'Fira Mono', monospace;
@@ -48,30 +103,37 @@ export default function ContactPage({ ...restProps }) {
   return (
     <Layout {...restProps}>
       <SEO title="Contact" />
-      <section>
+      <StyledStarCanvas coveredElementSelector="body"></StyledStarCanvas>
+      <Section>
         <Title>Contact</Title>
-        <Paragraph>
-          Let’s work together and build something out of this world!
-        </Paragraph>
-        <Form action="/" id="contact-form" method="POST" name="contact" netlify>
-          <FormGroup>
-            <Label htmlFor="name">Name</Label>
-            <Input id="name" form="contact-form" type="text" required />
-          </FormGroup>
-          <FormGroup>
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" form="contact-form" type="email" required />
-          </FormGroup>
-          <FormGroup>
-            <Label htmlFor="message">Message</Label>
-            <Input as="textarea" id="message" form="contact-form" required />
-          </FormGroup>
-          <BlockButton form="contact-form" type="submit">
-            <StyledIcon icon={faSatelliteDish} />
-            Transmit
-          </BlockButton>
-        </Form>
-      </section>
+        <Container>
+          <Paragraph>Let’s build something out of this world.</Paragraph>
+          <Form
+            action="/"
+            id="contact-form"
+            method="POST"
+            name="contact"
+            netlify
+          >
+            <FormGroup>
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" form="contact-form" type="text" required />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" form="contact-form" type="email" required />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="message">Message</Label>
+              <Input as="textarea" id="message" form="contact-form" required />
+            </FormGroup>
+            <BlockButton form="contact-form" type="submit">
+              <StyledIcon icon={faSatelliteDish} />
+              Transmit
+            </BlockButton>
+          </Form>
+        </Container>
+      </Section>
     </Layout>
   );
 }
