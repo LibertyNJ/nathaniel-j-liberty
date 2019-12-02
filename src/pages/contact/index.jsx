2f537c9cee158fe741f19ea7cc053b4f0a4b3204
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2019-12-02 10:07:19 -0500

    Add hidden input for netlify form-name

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -107,17 +107,36 @@ export default function ContactPage({ ...restProps }) {
           method="POST"
           name="contact"
         >
+          <input type="hidden" name="form-name" value="contact" />
           <FormGroup>
             <Label htmlFor="name">Name</Label>
-            <Input id="name" form="contact-form" name="name" type="text" required />
+            <Input
+              id="name"
+              form="contact-form"
+              name="name"
+              type="text"
+              required
+            />
           </FormGroup>
           <FormGroup>
             <Label htmlFor="email">Email</Label>
-            <Input id="email" form="contact-form" name="email" type="email" required />
+            <Input
+              id="email"
+              form="contact-form"
+              name="email"
+              type="email"
+              required
+            />
           </FormGroup>
           <FormGroup>
             <Label htmlFor="message">Message</Label>
-            <Input as="textarea" id="message" form="contact-form" name="message" required />
+            <Input
+              as="textarea"
+              id="message"
+              form="contact-form"
+              name="message"
+              required
+            />
           </FormGroup>
           <BlockButton form="contact-form" size="large" type="submit">
             <StyledIcon icon={faSatelliteDish} />
