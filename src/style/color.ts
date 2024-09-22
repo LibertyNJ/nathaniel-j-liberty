963a082cd501f56fa7fab04a5aefea4057b666b2
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2024-09-22 13:35:22 -0400

    Adjust color names

--- src/style/color.ts
+++ src/style/color.ts
@@ -1,7 +1,9 @@
 const BLACK = '#000';
+const LIGHT_BLACK = '#1a1a1a';
 const DARK_GRAY = '#333';
-const MEDIUM_GRAY = '#808080';
+const GRAY = '#808080';
 const LIGHT_GRAY = '#ccc';
+const DARK_WHITE = '#e6e6e6';
 const WHITE = '#fff';
 
 const gitHub = {
@@ -16,23 +18,25 @@ export const color = {
   black: BLACK,
   body: DARK_GRAY,
   darkGray: DARK_GRAY,
+  darkWhite: DARK_WHITE,
   gitHub,
+  lightBlack: LIGHT_BLACK,
   lightGray: LIGHT_GRAY,
   linkedIn,
-  mediumGray: MEDIUM_GRAY,
+  mediumGray: GRAY,
   white: WHITE,
 };
 
 export const darkTheme = {
   base: BLACK,
   contrast: WHITE,
-  hover: MEDIUM_GRAY,
+  hover: GRAY,
 };
 
 export const lightTheme = {
   base: WHITE,
   contrast: BLACK,
-  hover: MEDIUM_GRAY,
+  hover: GRAY,
 };
 
 export function getColorScheme(): ColorScheme {
