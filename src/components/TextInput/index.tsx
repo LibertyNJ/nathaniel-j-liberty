b399064ae11b620acf3b01b409ee47b25a85ee4b
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2024-09-22 08:41:59 -0400

    Extract ternary expression from TextInput JSX

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -5,20 +5,23 @@ import Props from './Props';
 import Textarea from './Textarea';
 
 export default function TextInput(props: Props) {
+  const input =
+    props.type === 'textarea' ? (
+      <Textarea
+        id={props.id}
+        name={props.id}
+        required={props.required}
+        rows={props.rows}
+        spellCheck
+      />
+    ) : (
+      <Input id={props.id} name={props.id} required={props.required} />
+    );
+
   return (
     <Container>
       <Label htmlFor={props.id}>{props.label}</Label>
-      {props.type === 'textarea' ? (
-        <Textarea
-          id={props.id}
-          name={props.id}
-          required={props.required}
-          rows={props.rows}
-          spellCheck
-        />
-      ) : (
-        <Input id={props.id} name={props.id} required={props.required} />
-      )}
+      {input}
     </Container>
   );
 }
